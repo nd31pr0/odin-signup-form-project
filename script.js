416d8a00c2ba54eb72cb686558a5c822811d6969
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-25 11:47:35 +0100

    add border to password and confirmPassword input

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 const validate = function (){
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm-password").value;
-    if (password !== confirmPassword){
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirm-password");
+    if (password.value !== confirmPassword.value){
         document.getElementById("pass-err").innerText = "*Passwords do not match";
         console.log("Passwords do not match")
+        password.style.border = "2px solid red";
+        confirmPassword.style.border = "2px solid red";
     }
 };
